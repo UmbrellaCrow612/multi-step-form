c562c55fa24b183f72ddaf6abf6f91651423573f
yousaf wazir <umbrellaman612@gmail.com>
2023-03-03 10:42:45 +0000

    doing add ons

--- src/pages/add-ons.tsx
+++ src/pages/add-ons.tsx
@@ -4,7 +4,14 @@ export default function Page() {
   return (
     <div className="grid min-h-screen place-content-center bg-[#d6d9e6] bg-opacity-80">
       <Form previousPageUrl="/plan" nextPageUrl="/summary">
-        <div>Add ons</div>
+        <div className="h-full pt-6 pl-20">
+          <h1 className="text-3xl font-bold leading-tight">Pick Add-ons</h1>
+          <p className="mt-3 text-sm text-gray-400">
+            Add-ons help enhance your gaming experience
+          </p>
+
+          
+        </div>
       </Form>
     </div>
   );
